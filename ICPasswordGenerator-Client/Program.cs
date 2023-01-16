992570b1f66ff6d8e915bd2504275ff5622379dd
IsraelChidera <israelchidera54@gmail.com>
2023-01-16 10:19:55 +0100

    Add Project

---
+++ ICPasswordGenerator-Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using ICPasswordGenerator;
+
+namespace ICPasswordGenerator_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            GeneratePassword ic = new();
+            ic.DisplayGenerate();
+        }
+    }
+}
---
+++ ICPasswordGenerator/GeneratePassword.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography;
+
+namespace ICPasswordGenerator
+{
+    public class GeneratePassword
+    {     
+        private string? _passwordLength;        
+
+        private string? GetPasswordLength {
+            get { return _passwordLength; }            
+            set { _passwordLength = value; }
+        }
+
+        public void GenerateRandomCharacters()
+        {
+            Console.WriteLine("Enter your desired password length");
+            GetPasswordLength = Console.ReadLine();
+
+            int passwordLength;
+            while (!int.TryParse(GetPasswordLength, out passwordLength))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error!\nPlease enter a valid number");
+                Console.ResetColor();
+
+                Console.WriteLine("Enter your desired password length");
+                GetPasswordLength = Console.ReadLine();                
+            }
+
+            Console.WriteLine($"Password l: {passwordLength}");
+
+            char[] characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=".ToCharArray();
+            char[] password = new char[passwordLength];
+            using (RandomNumberGenerator randomGenerator = RandomNumberGenerator.Create())
+            {
+                byte[] randomBytes = new byte[passwordLength];
+
+                randomGenerator.GetBytes(randomBytes);
+
+                for (int i = 0; i < passwordLength; i++)
+                {
+                    int randomIndex = randomBytes[i] % characters.Length;
+                    password[i] = characters[randomIndex];
+                }
+
+            }
+            Console.WriteLine($"Here is your password: {new string(password)}");            
+
+        }
+
+        public void DisplayGenerate()
+        {
+            GenerateRandomCharacters();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
